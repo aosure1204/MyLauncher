3627b61191a9c197d6f4ee8a5bf07df3b7984096
grace <grace.ai@wedesignhk.com>
2018-12-06 18:39:41 +0800

    First commit: only create project, without any function

---
+++ one_scroll/src/main/java/com/example/wedesign/launcherapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.wedesign.launcherapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SlidingDrawer;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
