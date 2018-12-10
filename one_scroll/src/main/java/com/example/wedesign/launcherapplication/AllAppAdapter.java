5c79a8d8e91c81624d2a2f76b870c5188317fb6b
grace <grace.ai@wedesignhk.com>
2018-12-10 18:34:31 +0800

    commit for learn git

--- one_scroll/src/main/java/com/example/wedesign/launcherapplication/AllAppAdapter.java
+++ one_scroll/src/main/java/com/example/wedesign/launcherapplication/AllAppAdapter.java
@@ -1,4 +1,36 @@
 package com.example.wedesign.launcherapplication;
 
-public class AllAppAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AllAppAdapter extends RecyclerView.Adapter<AllAppAdapter.ViewHolder> {
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+    public AllAppAdapter(int[] iconIds, String[] titleStrs) {
+        super();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
